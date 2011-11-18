83c9aafd8c222cd1a9fd8c6576886172bd32b9fb
Cliff Maury <cliff.maury@gmail.com>
2011-11-18 20:58:17 +0100

    Initial commit for submodule puppet-jenkins from Geppeto

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: puppet-jenkins
+#
+# This module manages puppet-jenkins
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class puppet-jenkins {
+
+
+}
